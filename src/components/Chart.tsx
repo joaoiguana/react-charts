44c0343ea1b117a208eef09a68fcdec802e85990
joao garcia <jiboiadamor@gmail.com>
2022-12-07 10:53:03 +0000

    created var options

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -26,7 +26,28 @@ const defaultOptions = {
       opacityFrom: 0.4,
       opacityTo: 0,
       shadeIntensity: 0.2,
-    }
+    },
+  },
+};
+
+var options = {
+  ...defaultOptions,
+  colors: [colorPrimary, colorDefault],
+  grid: {
+    borderColor: "rgba(255, 255, 255, 0.08)",
+    padding: {
+      left: -10, right: 0, top: -16, bottom: -8
+    },
+    xaxis: { lines: { show: false, }, },
+    yaxis: { lines: { show: false, }, },
+  },
+  yaxis: { labels: { show: false, }, },
+  xaxis: {
+    labels: { show: false, },
+    axisBorder: { show: false, },
+    axisTicks: { show: false, },
+    crosshairs: { show: false, },
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
   }
 };
 
